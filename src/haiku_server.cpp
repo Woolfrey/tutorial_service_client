6399c4fcff14e59bccb1c68971bbdc286bb7411a
Jon <jonathan.woolfrey@gmail.com>
2024-02-27 14:48:42 +0000

    Saving work in progress

--- src/haiku_server.cpp
+++ src/haiku_server.cpp
@@ -1,12 +1,15 @@
-#include <iostream>
-#include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/string.hpp>                                                                  // String message type
-#include <tutorial_service_definition/srv/haiku.hpp>
+#include <rclcpp/rclcpp.hpp>                                                                        // ROS2 C++ libraries
+#include <tutorial_service_definition/srv/haiku.hpp>                                                // Custom service type
 
-
-using HaikuService = tutorial_service_definition::srv::Haiku;
- 
-void getline(const HaikuService::Request &request, HaikuService::Response &response)                                        
+using HaikuService = tutorial_service_definition::srv::Haiku;                                       // Makes code easier to read
+  
+/**
+ * Forward declaration of callback function for the `haiku_service`.
+ * @param request A request for the line number of the haiku (1, 2, or 3)
+ * @param response A string containing the line of the poem.
+ */
+void get_line(const std::shared_ptr<HaikuService::Request>   request,
+                    std::shared_ptr<HaikuService::Response> response)                                        
 {
      // For reference, Haiku.srv:
      //  # Request
@@ -15,27 +18,27 @@ void getline(const HaikuService::Request &request, HaikuService::Response &respo
      //  # Response
      //  string line
      
-     switch(request.line_number)
+     switch(request->line_number)
      {    
           case 1:
           {
-               response.line = "Worker bees can leave.";
+               response->line = "Worker bees can leave.";
                break;
           }
           case 2:
           {
-               response.line = "Even drones can fly away.";
+               response->line = "Even drones can fly away.";
                break;
           }
           case 3:
           {
-               response.line = "The Queen is their slave.";
+               response->line = "The Queen is their slave.";
                break;
           }
           default:
           {
-               response.line = "FLAGRANT REQUEST ERROR: Expected request of 1, 2, or 3 but yours was "
-                             + std::to_string(request.line_number);
+               response->line = "FLAGRANT REQUEST ERROR: Expected request of 1, 2, or 3 but yours was "
+                             + std::to_string(request->line_number);
                break;
           }
      }
@@ -48,25 +51,16 @@ int main(int argc, char **argv)
 {
      rclcpp::init(argc,argv);                                                                       // Start up ROS2
      
-     rclcpp::Node node("haiku_server");                                                             // Create node with name `haiku_server`
+     std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("haiku_server");                // Needs to be a shared_ptr so we can spin() it later
+    
+     rclcpp::Service<HaikuService>::SharedPtr service =
+     node->create_service<HaikuService>("haiku_service", &get_line);                                // Create service from this node, attach callback
 
-     rclcpp::Service<HaikuService>::SharedPtr srvc = node.create_service<HaikuService>("haiku_service", &getline);
-
-/*
-  rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service =
-    node->create_service<example_interfaces::srv::AddTwoInts>("add_two_ints", &add);
-
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to add two ints.");
-
-  rclcpp::spin(node);
-  rclcpp::shutdown();
-   
-     std::cout << "Worker bees can leave.\n";
-     std::cout << "Even drones can fly away.\n";
-     std::cout << "The Queen is their slave.\n";
-     */
+     RCLCPP_INFO(node->get_logger(), "Ready to read you an haiku.");                                // Inform the user
+     
+     rclcpp::spin(node);                                                                            // Run this node indefinitely
      
      rclcpp::shutdown();                                                                            // Shut down ROS2
-     
+
      return 0;                                                                                      // No issues with main
 }
